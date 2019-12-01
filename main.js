332c4c435f6119f7e996eb42710c86d108ab7125
maksim-korzhov <korzhov.m.s@yandex.ru>
2019-12-01 16:06:14 +0300

    Generator with throw

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Show each node name from the element
+// 1. Show each node name from the element
 function* DomTraversal(element) {
     yield element;
     element = element.firstElementChild;
@@ -15,7 +15,7 @@ for(let item of domTraversalIterator) {
 }
 
 
-// Send and get data from generator
+// 2. Send and get data from generator
 function* printTwoNamesWithColor(color) {
     const firstPersonName = yield (`Tailer ${color}`);
 
@@ -23,5 +23,21 @@ function* printTwoNamesWithColor(color) {
 }
 
 const personIterator = printTwoNamesWithColor("gray");
-console.log("First Person:", personIterator.next());
-console.log("Second Person:", personIterator.next("Sarah"));
+// console.log("First Person:", personIterator.next());
+// console.log("Second Person:", personIterator.next("Sarah"));
+
+
+// 3. Throw an exception in the generator
+function* generatorWithException() {
+    try {
+        yield "Show some information";
+        yield "Some additional information";
+        console.log("This will never show up");
+    } catch(e) {
+        console.log("An error has been occured with the message:", e);
+    }
+}
+const exceptionIterator = generatorWithException();
+console.log("First line", exceptionIterator.next());
+exceptionIterator.throw("Catch this");
+console.log("Event if there 2 yields, after we have called throw we would have received end of the iterator", exceptionIterator.next());
